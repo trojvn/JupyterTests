008e0b782224c651e1fbf2dc239fa68b5af70255
trojvn <mrx.trojan@gmail.com>
2023-05-15 11:28:51 +0300

    характеристики

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ tg = Telegram(
     phone="+6283836372576",  # you can pass 'bot_token' instead
     database_encryption_key=b"f7b0a677e5078fefb10df0d0f6a253aa3e98eb88dc11d5e379ff6c9da84b77ba",
     files_directory="./tdlib2/",
+    device_model="Samsung SM-T713",
+    application_version="8.4.1 (2522)",
+    system_version="SDK 27",
 )
 tg.login()
 
